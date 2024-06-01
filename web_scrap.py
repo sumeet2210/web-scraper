8933a1b7424f8a166ba449f9f08417df103fce5c
sumeet2210 <sumeetmohanty707@GMAIL.COM>
2024-06-01 16:05:34 +0530

    Update web_scrap.py

--- web_scrap.py
+++ web_scrap.py
@@ -2,28 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-
 def scrape_wikipedia(url):
     response = requests.get(url)
     if response.status_code != 200:
-        raise Exception(f"Failed to load page {url}")
-
+        raise Exception(f"Unable to access page: {url}")
+    
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Extract Page Title
     title = soup.find('h1', id="firstHeading").text
 
-    # Extract First Paragraph
     first_paragraph = soup.find('div', class_='mw-parser-output').find('p').text
 
-    # Extract External Links
     external_links = []
     for link in soup.find_all('a', href=True):
         href = link['href']
         if href.startswith('http'):
             external_links.append(href)
 
-    # Extract Images and their alt text or captions
     images = []
     for img in soup.find_all('img'):
         img_url = urljoin(url, img['src'])
@@ -36,4 +31,13 @@ def scrape_wikipedia(url):
         'external_links': external_links,
         'images': images
     }
+
+if __name__ == "__main__":
+    wikipedia_url = "https://en.wikipedia.org/wiki/Web_scraping"
+    data = scrape_wikipedia(wikipedia_url)
+    
+    print(f"Title: {data['title']}\n")
+    print(f"First Paragraph: {data['first_paragraph']}\n")
+    print(f"External Links: {data['external_links']}\n")
+    print(f"Images: {data['images']}\n")
 
